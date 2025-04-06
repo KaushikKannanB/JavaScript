809ddd71b539863b03ebc0a61400ea632fd2c674
Kaushik <107951957+kaushikswift@users.noreply.github.com>
2025-04-06 16:08:00 +0530

    added task8: spa using hash

---
+++ task8/script.js
@@ -0,0 +1,15 @@
+const app =  document.getElementById("app");
+const routes = {
+    home: `<h2>🏠 Home</h2><p>Welcome to the homepage!</p>`,
+    about: `<h2>ℹ️ About</h2><p>This is a simple SPA using hash routing.</p>`,
+    contact: `<h2>📞 Contact</h2><p>Reach us at example@email.com.</p>`
+};
+
+function renderRoute()
+{
+    const hash = window.location.hash.substring(1);
+    app.innerHTML = routes[hash];
+}
+
+window.addEventListener('hashchange', renderRoute);
+window.addEventListener('load', renderRoute);
